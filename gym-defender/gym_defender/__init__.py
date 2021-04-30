72e13f6265c387e23d841933bae0d5c9144be230
Caspar-Baumeister <caspar.meister@hotmail.de>
2021-04-30 11:54:31 +0200

    added v0

--- gym-defender/gym_defender/__init__.py
+++ gym-defender/gym_defender/__init__.py
@@ -1,7 +1,7 @@
 from gym.envs.registration import register
 
 register(
-    id='defender',
+    id='defender-v0',
     entry_point='gym_defender.envs:Defender'
     #,kwargs={'K':20, 'initial_potential':0.8}
 )
