cf0dc61ffac83f842fe3808cb387324ce28bbc9a
Caspar-Baumeister <caspar.meister@hotmail.de>
2021-04-30 12:33:49 +0200

    Überaschung?

--- gym-defender/setup.py
+++ gym-defender/setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(name='gym_defender',
-      version='0.0.2',
+      version='0.0.3',
       install_requires=['gym']  # And any other dependencies defender needs
 )  
+print('Überaschung')
